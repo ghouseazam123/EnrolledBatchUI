287f7bd5c62806bd7813d16d66f0c40a3f24999a
Ghouse Azam <ghouseazam_s@nexteducation.in>
2026-02-11 17:51:40 +0530

    Initial Commit

---
+++ EnrolledBatchesUI/EnrolledBatchesUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnrolledBatchesUIApp.swift
+//  EnrolledBatchesUI
+//
+//  Created by next on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct EnrolledBatchesUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
